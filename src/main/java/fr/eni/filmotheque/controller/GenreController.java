a999e0d59dfea4993d80d3c035ea5eb4d089b3da
apetitpas2022 <alicia.petitpas@gmail.com>
2024-03-15 11:59:03 +0100

    Genre (création & update)

--- src/main/java/fr/eni/filmotheque/controller/GenreController.java
+++ src/main/java/fr/eni/filmotheque/controller/GenreController.java
@@ -1,6 +1,7 @@
 package fr.eni.filmotheque.controller;
 
 import fr.eni.filmotheque.bll.IFilmService;
+import fr.eni.filmotheque.bo.Film;
 import fr.eni.filmotheque.bo.Genre;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,14 +17,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/genres")
 public class GenreController {
     @Autowired
     private IFilmService filmService;
 
     List<Genre> listeGenres = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("/genres")
     public String getGenres(Model model) {
         model.addAttribute("genre", new Genre());
         listeGenres = filmService.consulterGenres();
@@ -31,7 +32,7 @@ public class GenreController {
         return "genres";
     }
 
-    @PostMapping
+    @PostMapping("/genres")
     public String postGenres(@Valid Genre genre, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             listeGenres = filmService.consulterGenres();
@@ -42,6 +43,49 @@ public class GenreController {
         genre.setId((long) (listeGenres.size() + 1));
 
         listeGenres.add(genre);
+
+        return "redirect:/genres";
+    }
+
+    @GetMapping("/genreCreation")
+    public String getCreationGenre(Model model) {
+        model.addAttribute("genre", new Genre());
+
+        return "genreCreation";
+    }
+
+    @PostMapping("/genreCreation")
+    public String postCreationGenre(@Valid Genre genre, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("genre", new Genre());
+            return "genreCreation";
+        }
+        listeGenres = filmService.consulterGenres();
+
+        genre.setId((long) (listeGenres.size() + 1));
+        listeGenres.add(genre);
+
+        return "redirect:/genres";
+    }
+
+    @GetMapping("/genreUpdate/{id}")
+    public String getUpdateGenre(@PathVariable("id") long id, Model model) {
+        model.addAttribute("genre", filmService.consulterGenreParId(id));
+
+        return "genreCreation";
+    }
+
+    @PostMapping("/genreUpdate/{id}")
+    public String postUpdateGenre(@PathVariable("id") long id, @Valid Genre genre, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("genre", filmService.consulterGenreParId(id));
+            return "genreCreation";
+        }
+
+        Genre genreAUpdate = filmService.consulterGenreParId(id);
+        genreAUpdate.setTitre(genre.Titre);
+
+        listeGenres = filmService.consulterGenres();
 
         return "redirect:/genres";
     }
